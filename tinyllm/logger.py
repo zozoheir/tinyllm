b8473c5e338150bafe8afe1f3eff6cd5f86e9aa2
othmanezoheir <zozoheir@umich.edu>
2023-07-24 15:00:37 -0400

    working credit analysis example with logging

--- tinyllm/logger.py
+++ tinyllm/logger.py
@@ -1,18 +1,17 @@
 import logging
 from logging import StreamHandler
 
-
 def get_logger(name: str,
                handlers=[StreamHandler()],
                format='%(levelname)s - %(asctime)s - msg: %(message)s',
                level=logging.INFO):
     logger = logging.getLogger(name)
+    logger.setLevel(level)
     root_logger = logging.getLogger()
     root_logger.handlers = []
     logger.handlers = []
     for handler in handlers:
         handler.setFormatter(logging.Formatter(format))
-        handler.setLevel(level)
         logger.addHandler(handler)
         logger.info("Logger initialized")
     return logger
