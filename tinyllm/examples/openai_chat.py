3330f6f6665c8e503404243a7866ad23e18e058c
othmanezoheir <zozoheir@umich.edu>
2023-07-25 00:13:48 -0400

    added comedian prompt template example

--- tinyllm/examples/openai_chat.py
+++ tinyllm/examples/openai_chat.py
@@ -1,24 +1,29 @@
 import asyncio
 
 from tinyllm.functions.llms.openai_chat import OpenAIChat
-
-model_name = 'gpt-3.5-turbo'
-messages = [
-    {
-        "role": "system",
-        "content": "Summarize content you are provided with for a second-grade student."
-    },
-    {
-        "role": "user",
-        "content": "Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass one-thousandth that of the Sun, but two-and-a-half times that of all the other planets in the Solar System combined. Jupiter is one of the brightest objects visible to the naked eye in the night sky, and has been known to ancient civilizations since before recorded history. It is named after the Roman god Jupiter.[19] When viewed from Earth, Jupiter can be bright enough for its reflected light to cast visible shadows,[20] and is on average the third-brightest natural object in the night sky after the Moon and Venus."
-    }
-]
+from tinyllm.functions.prompts.openai_chat.system import OpenAISystemMessage
+from tinyllm.functions.prompts.template import OpenAIPromptTemplate
+from tinyllm.functions.prompts.user_input import OpenAIUserMessage
 
 openai_chat = OpenAIChat(name='openai_chat',
                          model_name='gpt-3.5-turbo',
                          temperature=0,
                          n=1)
 
-response = asyncio.run(openai_chat(messages=messages))
+system_role_comedian = OpenAISystemMessage(name="Role",
+                                           content="You are a comedian. Given a subject, you generate a joke about it.")
 
-print(response)
+comedian_chat_template = OpenAIPromptTemplate(name="Loan Classifier Template",
+                                              sections=[
+                                                  system_role_comedian,
+                                                  OpenAIUserMessage(name="name"),
+                                              ])
+
+async def main():
+
+    messages = await comedian_chat_template(message="Subject: waking up grumpy\n")
+    response = await openai_chat(**messages)
+    print(response)
+
+if __name__ == '__main__':
+    asyncio.run(main())
