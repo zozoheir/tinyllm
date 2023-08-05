1c31379a51c0b3f4ca3a7257804d780ce90085e4
othmanezoheir <zozoheir@umich.edu>
2023-08-05 02:05:20 -0400

    langfuse auth as env

--- tinyllm/llm_trace.py
+++ tinyllm/llm_trace.py
@@ -1,9 +1,11 @@
+import os
+
 from langfuse import Langfuse
 from langfuse.api.model import CreateTrace, CreateGeneration, UpdateGeneration, CreateSpan, UpdateSpan
 
 langfuse_client = Langfuse(
-    public_key="pk-lf-3b6b0cb7-13c8-419b-adc6-b84dc9069021",
-    secret_key="sk-lf-2f5aafe7-be17-4d54-af7f-46bb60e58c4c",
+    public_key=os.environ['LANGFUSE_PUBLIC_KEY'],
+    secret_key=os.environ['LANGFUSE_SECRET_KEY'],
     host="https://cloud.langfuse.com/"
 )
 
