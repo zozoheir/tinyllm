fd6046487dc32ed637dd0915460c8bf4b0144bbf
zozoheir <zozoheir@umich.edu>
2023-12-14 19:29:27 -0500

    Bump version: 1.0.5 → 1.0.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tinyllm',
-      version='1.0.5',
+      version='1.0.6',
       description='Development and management infrastructure for LLM applications',
       packages=find_packages(),
       install_requires=[
